434f436ab1f4249bda33cad7ea39ef1da3636cff
Domum Digital <josh@domumdigital.com>
2025-03-25 22:09:39 -0400

    Update ExampleExternalContract.sol

--- challenge-1/ExampleExternalContract.sol
+++ challenge-1/ExampleExternalContract.sol
@@ -1,1 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20; //Do not change the solidity version as it negatively impacts submission grading
+
+// @Dev Built by Domum Digital. Est. 2021.
+// Originally deployed at 0xb35338C074A8cFcb46aA56424cF4DA48d888CC6f
+
+contract ExampleExternalContract {
+    bool public completed;
+
+    function complete() public payable {
+        completed = true;
+    }
+}
 
