521054b10b1f353478c2a68cd25ef33f8ac9d3fd
Domum Digital <josh@domumdigital.com>
2025-03-26 23:30:58 -0400

    Update YourToken.sol

--- challenge-2/YourToken.sol
+++ challenge-2/YourToken.sol
@@ -1,4 +1,3 @@
-
 pragma solidity 0.8.20; //Do not change the solidity version as it negatively impacts submission grading
 // SPDX-License-Identifier: MIT
 
@@ -11,6 +10,5 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract YourToken is ERC20 {
     constructor() ERC20("Gold", "GLD") {
         _mint(0xF3242e3EF228820E2fc84232B6837eB92f887017, 1000 * 10 ** 18);
-        // ^ Using our LocalHost burner address to mint the initial supply to.
     }
 }
