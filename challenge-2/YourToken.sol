c6791e8d0fd4ef98107a110291091849f4ecad59
Domum Digital <josh@domumdigital.com>
2025-03-26 23:08:21 -0400

    Update YourToken.sol

--- challenge-2/YourToken.sol
+++ challenge-2/YourToken.sol
@@ -2,6 +2,8 @@
 pragma solidity 0.8.20; //Do not change the solidity version as it negatively impacts submission grading
 // SPDX-License-Identifier: MIT
 
+// @dev Built by Domum Digital. Est. 2021.
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 // learn more: https://docs.openzeppelin.com/contracts/4.x/erc20
